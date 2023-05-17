dda0345b372f094ef71276cc7a40cb6a8cc2932e
klaire0202 <irene20030202@gmail.com>
2023-05-18 00:38:31 +0800

    第十三週的程式

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -1,16 +1,44 @@
-///Week12-4_keyboard_mouse
-///要能用 mouse 來移動，同時要解決存讀檔的問題
 #include <stdio.h>///要檔案的Input/Output
 #include <GL/glut.h>
+
+#include "glm.h"///week13_step02-2
+///再把glm.cpp 在左邊 Add files 加進去
+GLMmodel * head = NULL;///week13_step02-2
+GLMmodel * body = NULL;///week13_step02-2
+GLMmodel * uparmR = NULL;
+GLMmodel * lowarmR = NULL;
+
+int show[4] = {1,0,0,0};///week13_step03-1
+
 float teapotX=0,teapotY=0;
 FILE * fout = NULL; ///step02-1
 FILE * fin = NULL; ///step02-1
+
+void keyboard(unsigned char key,int x,int y)
+{
+    if(key=='0') show[0] = ! show[0];
+    if(key=='1') show[1] = ! show[1];
+    if(key=='2') show[2] = ! show[2];
+    if(key=='3') show[3] = ! show[3];
+    glutPostRedisplay();
+}
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    if(head==NULL)
+    {
+        head = glmReadOBJ("model/head.obj");///week13_step02-2
+        body = glmReadOBJ("model/body.obj");
+        uparmR = glmReadOBJ("model/uparmR.obj");
+        lowarmR = glmReadOBJ("model/lowarmR.obj");
+        ///glmUnitize(head);///week13_step02-2 之後改
+    }
     glPushMatrix();
-        glTranslatef(teapotX,teapotY,0);
-        glutSolidTeapot(0.3);
+        glScalef(0.3,0.3,0.3);///week13_step02-3
+        if(show[0]) glmDraw(head,GLM_MATERIAL);///week13_step02-3
+        if(show[1]) glmDraw(body,GLM_MATERIAL);///week13_step02-2
+        if(show[2]) glmDraw(uparmR,GLM_MATERIAL);///week13_step02-3
+        if(show[3]) glmDraw(lowarmR,GLM_MATERIAL);///week13_step02-3
     glPopMatrix();
     glutSwapBuffers();
 }
@@ -25,7 +53,7 @@ void mouse(int button,int state,int x,int y)
     }
     display();
 }
-void keyboard(unsigned char key, int x,int y)
+/*void keyboard(unsigned char key, int x,int y)
 {
     if(fin==NULL)
     {
@@ -34,12 +62,12 @@ void keyboard(unsigned char key, int x,int y)
     }
     fscanf(fin,"%f%f",&teapotX,&teapotY);
     display();
-}
+}*/
 int main(int argc, char**argv)
 {
     glutInit( &argc, argv );
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
-    glutCreateWindow("week12");
+    glutCreateWindow("week13");
 
     glutDisplayFunc(display);
     glutKeyboardFunc(keyboard);///step02-2
